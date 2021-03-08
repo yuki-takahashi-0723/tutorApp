62e53c2e8154272df4362259b16264b596677a11
yuki-takahashi-0723 <honehonehone0723@gmail.com>
2021-03-08 21:03:24 +0900

    サインイン、サインアップページのスタイリング

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -4,6 +4,54 @@ import { AuthContext } from '../AuthSearvis'
 import { auth } from '../firebase/config'
 import { InputArea, PrimaryButton } from '../uikit'
 import * as H from 'history'
+import styled from 'styled-components'
+import humsterImage from '../img/IMG_0931.jpeg'
+const SignInWrap = styled.div`
+    margin:0;
+    padding:0;
+    width:100vw;
+    height:100vh;
+    position: relative;
+    width: 100%;
+    min-height: 100vh;
+    background: url(${humsterImage})
+    center / cover;
+`
+const SignInForm = styled.div`
+    width:80vw;
+    height:60vh;
+    text-align:center;
+    background-color:rgba(255,255,255,0.5); //後ほど変更を行う
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    @media (min-width:600px){
+      width:60vw;
+    }  
+    
+
+    
+`
+const Title = styled.h2`
+    margin:0 ;
+`
+
+const InputWrap = styled.div`
+    width:50vw;
+    margin:0 auto;
+    text-align:center;
+`
+const ButtonWrap = styled.div`
+    text-align:center;
+`
+
+
 
 type Props = {
     history : H.History
@@ -39,36 +87,45 @@ const SignIn:React.FC<Props> = ({history}) => {
     }
     
     return(
-        <>
-            <h2>サインイン</h2>
-            <InputArea
-                required = {true}
-                fullWidth={false}
-                multiline={false}
-                rows={1}
-                onChange={inputEmail}
-                value={email}
-                label={'メールアドレス'}
-                type={'Email'}
-            />
-            <InputArea
-                required = {true}
-                fullWidth={false}
-                multiline={false}
-                rows={1}
-                onChange={inputPassword}
-                value={password}
-                label={'パスワード'}
-                type={'Password'}
-            />
-           
-            <PrimaryButton
-                label={'サインイン'}
-                onClick={signInAciton}
+        <SignInWrap>
+            <SignInForm>
+                <Title>サインイン</Title>
+                <InputWrap>
+                    <div>
+                        <InputArea
+                            required = {true}
+                            fullWidth={true}
+                            multiline={false}
+                            rows={1}
+                            onChange={inputEmail}
+                            value={email}
+                            label={'メールアドレス'}
+                            type={'Email'}
+                        />
+                    </div>
+                    <div>
+                        <InputArea
+                            required = {true}
+                            fullWidth={true}
+                            multiline={false}
+                            rows={1}
+                            onChange={inputPassword}
+                            value={password}
+                            label={'パスワード'}
+                            type={'Password'}
+                        />
 
-            />
+                    </div>
+                    <ButtonWrap>
+                        <PrimaryButton
+                            label={'サインイン'}
+                            onClick={signInAciton}
+                        />
+                    </ButtonWrap>
+                </InputWrap>
+            </SignInForm>
 
-        </>
+        </SignInWrap>
     )
 }
 
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -4,6 +4,57 @@ import { InputArea, PrimaryButton } from '../uikit'
 import * as H from 'history'
 import { AuthContext } from '../AuthSearvis'
 import { Redirect } from 'react-router-dom'
+import styled from 'styled-components'
+import humsterImage from '../img/IMG_0931.jpeg'
+
+const SignUpWrap = styled.div`
+    margin:0;
+    padding:0;
+    width:100vw;
+    height:100vh;
+    position: relative;
+    width: 100%;
+    min-height: 100vh;
+    background: url(${humsterImage})
+    center / cover;
+`
+const SignUpForm = styled.div`
+    width:80vw;
+    height:60vh;
+    text-align:center;
+    background-color:rgba(255,255,255,0.5); //後ほど変更を行う
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    @media (min-width:600px){
+      width:60vw;
+    }  
+    
+
+    
+`
+const Title = styled.h2`
+    margin:0 ;
+`
+
+const InputWrap = styled.div`
+    width:50vw;
+    margin:0 auto;
+    text-align:center;
+`
+const ButtonWrap = styled.div`
+    text-align:center;
+`
+
+
+
+
 
 type Props = {
     history : H.History
@@ -45,44 +96,55 @@ const SignUp:React.FC<Props> = ({history}) => {
     }
     
     return(
-        <>
-            <h2>サインアップ</h2>
-            <InputArea
-                required = {true}
-                fullWidth={false}
-                multiline={false}
-                rows={1}
-                onChange={inputEmail}
-                value={email}
-                label={'メールアドレス'}
-                type={'Email'}
-            />
-            <InputArea
-                required = {true}
-                fullWidth={false}
-                multiline={false}
-                rows={1}
-                onChange={inputPassword}
-                value={password}
-                label={'パスワード'}
-                type={'Password'}
-            />
-            <InputArea
-                required = {true}
-                fullWidth={false}
-                multiline={false}
-                rows={1}
-                onChange={inputName}
-                value={name}
-                label={'企業名（仮称でも可）'}
-                type={'Text'}
-            />
-            <PrimaryButton
-                label={'サインアップ'}
-                onClick={signUpAciton}
-            />
-
-        </>
+        <SignUpWrap>
+            <SignUpForm>
+                <Title>サインアップ</Title>
+                <InputWrap>
+                    <div>
+                        <InputArea
+                            required = {true}
+                            fullWidth={false}
+                            multiline={false}
+                            rows={1}
+                            onChange={inputEmail}
+                            value={email}
+                            label={'メールアドレス'}
+                            type={'Email'}
+                        />
+                    </div>
+                    <div>
+                        <InputArea
+                            required = {true}
+                            fullWidth={false}
+                            multiline={false}
+                            rows={1}
+                            onChange={inputPassword}
+                            value={password}
+                            label={'パスワード'}
+                            type={'Password'}
+                        />
+                    </div>
+                    <div>
+                        <InputArea
+                            required = {true}
+                            fullWidth={false}
+                            multiline={false}
+                            rows={1}
+                            onChange={inputName}
+                            value={name}
+                            label={'企業名(仮名加)'}
+                            type={'Text'}
+                        />
+                    </div>
+                    <ButtonWrap>
+                        <PrimaryButton
+                            label={'サインアップ'}
+                            onClick={signUpAciton}
+                        />
+                    </ButtonWrap>
+                </InputWrap>
+            </SignUpForm>
+        </SignUpWrap>
     )
 }
 
